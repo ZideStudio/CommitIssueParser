2d28aac3ae0b370e1f211c3d6b3ddad867efce83
Jules Juul <julessorens@gmail.com>
2025-07-11 00:16:15 +0200

    chore: remove raycast-env.d.ts from gitignore

---
+++ raycast-env.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Primary Action - Set the default behavior when you select a commit name */
+  "onSelection": "paste" | "copypaste" | "copy",
+  /** Content Format - Applied format when you select a commit name */
+  "contentFormat": "text" | "lazygit" | "git",
+  /** Type Mode - Configure the mode of your commit types */
+  "typeMode": "text" | "gitmoji"
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `parse-issue-url` command */
+  export type ParseIssueUrl = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `parse-issue-url` command */
+  export type ParseIssueUrl = {}
+}
+
